38e908a594b372c20a7064f7dc9c7bfcab1d40a6
Code4Daze <jevon.fillette@gmail.com>
2022-05-23 17:24:00 -0400

    Add a hover function to each grid in the container
    Each grid that is hovered over will change to black.

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 let container = document.querySelector('#container');
 let containerSideLength = 600;
+let div;
 container.style.height = `${containerSideLength}px`;
 container.style.width = `${containerSideLength}px`;
 
 function createGrid(num) {
   let squareSide = (containerSideLength / num) - 2;
   for (let i = 0; i < (num * num); i++) {
-    let div = document.createElement('div');
+    div = document.createElement('div');
     div.classList.add('gridDiv');
     div.style.height = `${squareSide}px`;
     div.style.width = `${squareSide}px`;
     container.appendChild(div);
   }
+  let gridSquares = container.querySelectorAll('div');
+  gridSquares.forEach (gridSquare => gridSquare.addEventListener('mouseover', addColor));
 }
 
-createGrid(16);
+function addColor() {
+  this.style.background = 'black';
+}
+
+
+
+
+createGrid(24);

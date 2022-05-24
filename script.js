6766b43e9d6531bfb4ad77cb4abf27486b1f166c
Code4Daze <jevon.fillette@gmail.com>
2022-05-23 20:14:22 -0400

    Add reset button and show current dimensions

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ container.style.height = `${containerSideLength}px`;
 container.style.width = `${containerSideLength}px`;
 let customGridBtn = document.querySelector('.customGrid');
 let resetBtn = document.querySelector('.resetBtn');
+let currentDimensions = document.querySelector('.currentGrid');
+let currentColor = document.querySelector('.selection');
 
-customGridBtn.addEventListener('click', () => {
+customGridBtn.addEventListener('click', newGame);
+resetBtn.addEventListener('click', resetGame);
+
+function newGame() {
   let userInput = +prompt("Select the number of squares per side", "16");
-  console.log(userInput);
   if (userInput <= 100 && userInput > 0) {
     createGrid(userInput);
+    currentDimensions.textContent = `${userInput}x${userInput}`;
   } else if (userInput === 0) {
     return;
   } else {
     alert("Please choose a value that is between 1 and 100");
   }
-});
+}
 
 function createGrid(num) {
   while (container.hasChildNodes()) {
@@ -36,6 +41,12 @@ function createGrid(num) {
 function addColor() {
   // Random color - ie. Rainbow
   this.style.background = "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function resetGame() {
+  createGrid(16);
+  currentDimensions.textContent = "16x16";
+  currentColor.textContent = "Black";
 }
 
 // Default Size
